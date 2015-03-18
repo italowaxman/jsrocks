7717317afe14d30ed0aaf7621bf0d6f6525e08f5
Italo Waxman <italowaxman@gmail.com>
2015-03-18 20:14:50 -0300

    Search form and icon effects is fixed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {     
-	$('.button-post').hover(function() {
-		$(this).toggleClass('button-thunder');
+	$(".button-post").hover(function() {
+		$(this).toggleClass("button-thunder");
 	});
+
+	$(".input-search").hover(function () {
+	  $(".icon-search-white").toggleClass("icon-search-hover");
+	});
+
+	$(".input-search").on('focus blur', function(){
+    	$(".icon-search-white").toggleClass("icon-search-focus");
+    	$(".icon-search").toggleClass("hide-bg");
+	});
+
 });   
 
 
